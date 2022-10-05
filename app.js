834bc49d2944d9b8acd39df87da2a04f3cca9b09
MAS234 <miguel.martinez.angel.salazar@gmail.com>
2022-10-04 22:20:54 -0300

    nuevo

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ const menu = document.querySelector(".menu");
 boton.addEventListener("click",()=>{
     menu.classList.toggle("activo");
 })
+
+
+
+
+
+// CARROSEL
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
